375a4bccbf78a9d44fcc0084b1c5782ebc0bbe5c
jacklok <jack22hk@gmail.com>
2019-11-06 12:23:24 -0800

    test

--- okstupid/models.py
+++ okstupid/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.files.storage import FileSystemStorage
 
+
 fs = FileSystemStorage(location='/media/photos')
 
 # Create your models here.
